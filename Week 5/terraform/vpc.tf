4e7d2f3d02586fd70b07a6ab2ca3db67d82a7a8a
iampsrv <iampsrv@gmail.com>
2025-08-30 14:00:53 +0530

    Week 5 res

---
+++ Week 5/terraform/vpc.tf
@@ -0,0 +1,43 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "myvpc"
+  }
+}
+
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.myvpc.id
+
+  tags = {
+    Name = "myigw"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "my_subnet"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.myrt.id
+}
+
+resource "aws_route_table" "myrt" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myigw.id
+  }
+
+  tags = {
+    Name = "myrt"
+  }
+}
